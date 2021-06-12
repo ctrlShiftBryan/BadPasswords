f0f24a59d95b823755cbcc9f4a080ed7c71949a9
Bryan Arendt <bryanarendt@gmail.com>
2021-06-12 09:31:33 -0400

    update mix properties

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule Badpasswords.MixProject do
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      source_url: "https://github.com/ctrlShiftBryan/BadPasswords"
     ]
   end
 
@@ -16,6 +18,14 @@ defmodule Badpasswords.MixProject do
     [
       extra_applications: [:logger]
     ]
+  end
+
+  defp package do
+    %{
+      licenses: ["Apache-2.0"],
+      maintainers: ["Bryan Arendt"],
+      links: %{"GitHub" => "https://github.com/ctrlShiftBryan/BadPasswords"}
+    }
   end
 
   # Run "mix help deps" to learn about dependencies.
